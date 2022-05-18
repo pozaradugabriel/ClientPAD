588331cf3fc71526aece1c99a02657e8118cde77
Poza Radu <poza.radu@gmail.com>
2022-05-18 23:32:50 +0300

    added maps on homescreen

--- ArtExplorerClient/src/screens/HomeScreen.js
+++ ArtExplorerClient/src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is the homescreen.</Text>
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -14,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
