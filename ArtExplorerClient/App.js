77f5dab6a5fbf8e2ba287685bb1cede24a9bf500
Poza Radu <poza.radu@gmail.com>
2022-05-27 22:51:43 +0300

    created,designed signup page and added it to the nav stack:

--- ArtExplorerClient/App.js
+++ ArtExplorerClient/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -13,6 +14,11 @@ export default function App() {
           headerShown:false
         }
       }>
+        <Stack.Screen
+          name="singup_screen"
+          component={SignupScreen}
+        />
+
         <Stack.Screen
           name="login_screen"
           component={LoginScreen}
